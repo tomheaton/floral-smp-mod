99652294f2a0437aeb9e6d328a28004ff75e3f30
Tom Heaton <thomas.m.heaton@gmail.com>
2022-08-04 23:32:21 +0100

    fixes

--- src/main/java/dev/tomheaton/floralsmp/FloralSMP.java
+++ src/main/java/dev/tomheaton/floralsmp/FloralSMP.java
@@ -2,13 +2,7 @@ package dev.tomheaton.floralsmp;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FlowerBlock;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
