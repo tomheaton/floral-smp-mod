7840c774ba19b51b92d237ab63e333cea64143fe
Tom Heaton <thomas.m.heaton@gmail.com>
2022-08-22 23:41:51 +0100

    coding time

--- src/main/java/dev/tomheaton/floralsmp/FloralBlockItem.java
+++ src/main/java/dev/tomheaton/floralsmp/FloralBlockItem.java
@@ -1,7 +1,9 @@
 package dev.tomheaton.floralsmp;
 
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 
 public class FloralBlockItem extends BlockItem {
@@ -12,5 +14,10 @@ public class FloralBlockItem extends BlockItem {
 
     public FloralBlockItem(Block block) {
         super(block, new Item.Properties().tab(FloralSMP.floralItemGroup));
+    }
+
+    @Override
+    public InteractionResult place(BlockPlaceContext blockPlaceContext) {
+        return InteractionResult.FAIL;
     }
 }
